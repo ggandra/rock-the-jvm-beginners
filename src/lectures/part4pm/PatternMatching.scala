862f804ec9bb5d2a2f18e62e6ce1a1b9a91da424
ggandra <gandra.gabriel@hotmail.com>
2021-06-15 20:34:52 -0300

    Scala pattern matching

---
+++ src/lectures/part4pm/PatternMatching.scala
@@ -0,0 +1,64 @@
+package lectures.part4pm
+
+import scala.util.Random
+
+object PatternMatching extends App {
+
+  // switch on steroids
+  val random = new Random
+  val x = random.nextInt(10)
+
+  val description = x match {
+    case 1 => "the one"
+    case 2 => "double or nothing"
+    case 3 => "third time is the charm"
+    case _ => "something else"
+  }
+
+  println(x)
+  println(description)
+
+  // Decompose values
+  case class Person(name: String, age: Int)
+  val bob = Person("Bob", 20)
+
+  val greeting = bob match {
+    case Person(n, a) if a < 21 => s"Hi, my name is $n and i can't drink in the US"
+    case Person(n, a) => s"Hi, my name is $n and i am $a"
+    case _ => "I don't know who i am"
+  }
+
+  println(greeting)
+
+  // PM on sealed hierarchies
+  sealed class Animal
+  case class Dog(breed: String) extends Animal
+  case class Parrot(greeting: String) extends Animal
+
+  val animal: Animal = Dog("Terra Nova")
+  animal match {
+    case Dog(someBreed) => println(s"Matched a dog breed: $someBreed")
+  }
+
+  trait Expr
+  case class Number(n: Int) extends Expr
+  case class Sum(e1: Expr, e2: Expr) extends Expr
+  case class Product(e1: Expr, e2: Expr) extends Expr
+
+  def show(e: Expr): String = e match {
+    case Number(n) => s"$n"
+    case Sum(e1, e2) => show(e1) + " " + show(e2)
+    case Product(e1, e2) => {
+      def maybeShowParentheses(exp: Expr) = exp match {
+        case Product(_, _) => show(exp)
+        case Number(_) => show(exp)
+        case _ => s"( ${show(exp)} )"
+      }
+
+      maybeShowParentheses(e1) + " * " + maybeShowParentheses(e2)
+    }
+  }
+
+  println(show(Sum(Number(2), Number(3))))
+  println(show(Sum(Number(2), Number(3))))
+}
